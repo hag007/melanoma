7cc8aba5052cd55003f29c93a3b40ee9d29d9e71
Hagai Levi <hagailevi@localhost.localdomain>
2018-07-16 14:00:48 +0300

    env done

--- env/init/init.sh
+++ env/init/init.sh
@@ -1,11 +1,13 @@
-!#/bin/bash
-DIRECTORY = ../../../python27_test
+#!/bin/bash
+
+DIRECTORY="../../../python27"
 
 if [ ! -d "$DIRECTORY" ]; then
   mkdir $DIRECTORY
 fi
-
+ 
 virtualenv $DIRECTORY
-source $DIRECTORY/bin/activate.csh
+# csh $DIRECTORY/bin/activate.csh
+source $DIRECTORY/bin/activate
 pip install -r dependencies.txt
 deactivate
