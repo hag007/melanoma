10cbb673578da9a0676b43dc11d2699a7155e3f1
Hagai Levi <hagailevi@localhost.localdomain>
2018-07-31 22:02:26 +0300

    steiner tree done

--- steiner_tree.py
+++ steiner_tree.py
@@ -4,12 +4,6 @@ __copyright__ = "Copyright (C) 2016-2018, DV Klopfenstein, H Tang. All rights re
 __author__ = "DV Klopfenstein"
 
 import sys
-import time
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
-
-sys.path.insert(0, '../')
 import collections as cx
 from goatools.godag.consts import Consts
 from goatools.gosubdag.go_paths import GoPaths
@@ -36,14 +30,6 @@ from utils.string_ppi_dictionary import get_string_ppi_dict
 from utils.go_dictionary import get_go_dict
 import infra
 import pcst_fast
-import networkx as nx
-from utils.go_dictionary import get_go_dict
-from utils.prize_dictionary import get_prize_dict
-from utils.graph_influence_linear_th import linear_threshold
-
-INFLUENCE_DISTANCE_FACTOR = 0.7
-INFLUENCE_NODE_TH = 0.3
-INFLUENCE_N_STEPS=1000
 
 class WrHierGO(object):
 
@@ -78,8 +64,6 @@ class _WrHierPrt(object):
         self.concise_prt = 'concise' in obj.usrset
         self.indent = 'no_indent' not in obj.usrset
         self.go2geneids = obj.usrdct.get('go2geneids')
-        self.prize_dictionary= obj.usrdct.get('prize_dictionary')
-        self.G = obj.usrdct.get('G')
         # vars
         self.prt = prt
         self.edges = {}
@@ -100,24 +84,15 @@ class _WrHierPrt(object):
         if self.vertices.has_key(goid):
             self.vertices[goid]["weight"] += 1
             self.vertices[goid]["depth"].append(depth)
-            self.G.node[goid]["weight"] = self.G.node[goid]["weight"] + 1
-            self.G.node[goid]["depth"].append(depth)
-
         else:
-	    go2geneids = self.go2geneids
-            prize = self.prize_dictionary.get(goid, 0)
-            node_attr = {"name": ntgo.GO_name,
-                         "weight": 0,
-                         "NS": ntgo.NS,
-                         "depth": [depth],
-                         "L": ntgo.level,
-                         "D": ntgo.depth,
-                         "isleaf": len(ntobj.children) == 0,
-                         "obj": ntobj,
-                         "threshold": INFLUENCE_NODE_TH,
-                         "prize": prize}
-            self.G.add_node(goid, **node_attr)
-            self.vertices[goid] = node_attr
+            self.vertices[goid] = {"name": ntgo.GO_name,
+                                   "weight": 0,
+                                   "NS": ntgo.NS,
+                                   "depth": [depth],
+                                   "L" : ntgo.level,
+                                   "D" : ntgo.depth,
+                                   "isleaf" : len(ntobj.children) == 0,
+                                   "obj": ntobj}
 
         self.gos_printed.add(goid)
         # Do not extract hierarchy below this turn if it has already been printed
@@ -127,14 +102,12 @@ class _WrHierPrt(object):
         if self.max_indent is not None and depth > self.max_indent:
             return
         for child in ntobj.children:
-            if self.go2geneids.has_key(child.id):
+            if self.go2geneids.has_key(child.id) or True:
                 if self.edges.has_key("{}={}".format(goid, child.id)):
-                    self.G.edges[(goid, child.id)]["weight"] += 1
                     self.edges["{}={}".format(goid, child.id)]["weight"] += 1
                 else:
-                    self.G.add_edge(goid, child.id, **{"weight": 0, "influence": INFLUENCE_DISTANCE_FACTOR / float(len(ntobj.children))})
-                    self.edges["{}={}".format(goid, child.id)] = {"weight": 0}
-            self.ext_hier_rec(child.id, depth)
+                    self.edges["{}={}".format(goid, child.id)] = {"weight" : 0}
+                self.ext_hier_rec(child.id, depth)
 
 
     @staticmethod
@@ -157,14 +130,12 @@ class _WrHierPrt(object):
         prtfmt = prtfmt.replace('{D1:5} ', '')
         return prtfmt
 
-def extract_hier_all(gosubdag, out, root_term, go2geneids, prize_dictionary):
+def extract_hier_all(gosubdag, out, root_term, go2geneids):
     """write_hier.py: Prints the entire mini GO hierarchy, with counts of children."""
     out.write('\nTEST EXTRACTION: Print all hierarchies:\n')
-    objwr = WrHierGO(gosubdag,  go2geneids=go2geneids, prize_dictionary=prize_dictionary, G=nx.DiGraph())
+    objwr = WrHierGO(gosubdag,  go2geneids=go2geneids)
     obj = objwr.ext_hier_down(root_term, out)
-    seeds = prize_dictionary.keys()
-    layer_i_nodes = linear_threshold(obj.G, seeds, steps=INFLUENCE_N_STEPS)
-    return (obj.vertices, obj.edges, obj.G, layer_i_nodes)
+    return (obj.vertices, obj.edges)
 
 def fetch_go_hierarcy():
     obo_file_location = os.path.join(constants.GO_DIR, constants.GO_FILE_NAME)
@@ -235,7 +206,6 @@ def build_hierarcy():
     print "fetching ppi"
     go_edges = fetch_string_ppi_edges()
     go_dict = get_go_dict()
-    prize_dictionaty = get_prize_dict()
 
     go2geneids, geneids2go = fetch_go_hierarcy()
 
@@ -248,23 +218,19 @@ def build_hierarcy():
     out = file(os.path.join(constants.BASE_PROFILE, "output", "go_hierarcy.txt"), "w+")  # sys.stdout
     dict_result = {}
     for cur_term in ['GO:0005575']:
-        vertices, edges, G, layers = extract_hier_all(gosubdag, out, cur_term, go2geneids, prize_dictionaty)
+        vertices, edges = extract_hier_all(gosubdag, out, cur_term, go2geneids)
         dict_result[cur_term] = {"vertices": vertices, "edges": edges}
-        print layers
 
     count=0
     vertices_grid = []
-    vertices_grid_values = []
+    vertices_grid_values = []  
     vertices_prizes = []
     for k, v in dict_result['GO:0005575']['vertices'].iteritems():
         if dict_result['GO:0005575']['vertices'].has_key(k) \
                         and dict_result['GO:0005575']['vertices'][k]['isleaf']:
             vertices_grid.append(k)
             vertices_grid_values.append(v)
-            vertices_prizes.append(0)
-	    x = [x for x in layers if k in x]
-	    if len(x)>0:
-		vertices_prizes[-1] = (len(layers) - layers.index(x[0]))/float(len(layers))
+            vertices_prizes.append(1/float(10000000)) # 
             count+=1
     print "total vartices: {}".format(count)
 
@@ -279,10 +245,10 @@ def build_hierarcy():
             'vertices'].has_key(vertices[1]) and score > 10000 \
                 and dict_result['GO:0005575']['vertices'][vertices[0]]['isleaf'] and \
                 dict_result['GO:0005575']['vertices'][vertices[1]]['isleaf']:
-
+            
             cost = (len(go_dict[vertices[0]]["ENSP"])*len(go_dict[vertices[1]]["ENSP"]))
 	    cost_alt = (len(go2geneids[vertices[0]])*len(go2geneids[vertices[1]]))
-            # print "cost/alt: {}, {}:".format(cost, cost_alt)
+            # print "cost/alt: {}, {}:".format(cost, cost_alt)            
 	    if cost != 0:
 		cost=cost/float(score)
                 edges_grid.append([vertices_grid.index(vertices[0]), vertices_grid.index(vertices[1])])
@@ -295,10 +261,10 @@ def build_hierarcy():
     max_cost=np.max(edges_costs)
     for i, cur_cost in enumerate(edges_costs):
 	edges_costs[i] = cur_cost/(max_cost-min_cost)
-    percentiles = [np.percentile(edges_costs,x*10) for x in range(11)]
-    print "edge precentiles: {}".format(percentiles)
+    percentiles = [np.percentile(edges_costs,x*10) for x in range(11)]  
+    print "edge precentiles: {}".format(percentiles) 
     print np.min(edges_costs)
-    vertices_prizes = [percentiles[6]*x for x in vertices_prizes]
+    vertices_prizes = [0.0005 for x in vertices_prizes]
     edges_grid=np.array(edges_grid).astype(np.int64)
     vertices_prizes=np.array(vertices_prizes).astype(np.float64)
     edges_costs = np.array(edges_costs).astype(np.float64)
@@ -307,33 +273,14 @@ def build_hierarcy():
     pruning = 'strong' # 'none'
     verbosity_level = 0
     vertices, edges = pcst_fast.pcst_fast(edges_grid, vertices_prizes, edges_costs, root, num_clusters, pruning, verbosity_level)
-    G=nx.Graph()
-    # print vertices_prizes
-    # print edges_costs
-    # print "vertices"
-    # print [vertices_grid[x] for x in vertices]
-    # print [vertices_grid_values[x]["name"] for x in vertices]
-    c_values = {}
-    labels = {}
-    for cur_v in vertices:
-        cur_layer = [x for x in layers if vertices_grid[cur_v] in x]
-	level=len(layers)
-        if len(cur_layer) != 0:
-		level=	layers.index(cur_layer[0])
-	G.add_node(vertices_grid[cur_v],
-		**{"name": vertices_grid_values[cur_v]["name"], "level": level})
-        c_values[vertices_grid[cur_v]]=1-level/float(len(layers))
-        labels[vertices_grid[cur_v]] = vertices_grid_values[cur_v]["name"]
-    c_list = [c_values[x] for x in G.nodes()]
-    print [G.node[x] for x in G.nodes()]
-    # print "edges"
-    # print ["{}={}".format(vertices_grid[edges_grid[x][0]], vertices_grid[edges_grid[x][1]]) for x in edges]
-    # print ["{} = {}".format(vertices_grid_values[int(edges_grid[x][0])]["name"], vertices_grid_values[int(edges_grid[x][1])]["name"]) for x in edges]
-    for cur_e in edges:
-	G.add_edge(vertices_grid[edges_grid[cur_e][0]], vertices_grid[edges_grid[cur_e][1]])
-
-    nx.draw_networkx(G, cmap=plt.get_cmap('jet'), node_color=c_list, labels=labels, font_size=8)
-    plt.savefig(os.path.join(constants.OUTPUT_GLOBAL_DIR, "PCST_{}.png".format(time.time())))
+    print vertices_prizes
+    print edges_costs
+    print "vertices"
+    print [vertices_grid[x] for x in vertices]
+    print [vertices_grid_values[x]["name"] for x in vertices]
+    print "edges"
+    print ["{}={}".format(vertices_grid[edges_grid[x][0]], vertices_grid[edges_grid[x][1]]) for x in edges]
+    print ["{} = {}".format(vertices_grid_values[int(edges_grid[x][0])]["name"], vertices_grid_values[int(edges_grid[x][1])]["name"]) for x in edges]
 
 if __name__ == '__main__':
     print build_hierarcy()
